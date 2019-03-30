72c11da1bee93c3e1db4491445c53fa9ecaba5f4
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-03-30 15:09:39 -0400

    Updated unit test

--- tests/test_add_argument.hpp
+++ tests/test_add_argument.hpp
@@ -14,3 +14,18 @@ TEST_CASE("Add a simple argument", "[add_argument]") {
   REQUIRE(arguments["--foo"]->mRawValues.size() == 0);
   REQUIRE(arguments["--foo"]->mValues.size() == 0);
 }
+
+TEST_CASE("Add a simple argument with help", "[add_argument]") {
+  argparse::ArgumentParser program("test");
+  program.add_argument("--foo")
+    .help("input file");
+
+  auto arguments = program.get_arguments();
+  REQUIRE(arguments.size() == 1);
+  REQUIRE(arguments["--foo"] != nullptr);
+  REQUIRE(arguments["--foo"]->mNames == std::vector<std::string>{"--foo"});
+  REQUIRE(arguments["--foo"]->mHelp == "input file");
+  REQUIRE(arguments["--foo"]->mNumArgs == 1);
+  REQUIRE(arguments["--foo"]->mRawValues.size() == 0);
+  REQUIRE(arguments["--foo"]->mValues.size() == 0);
+}
