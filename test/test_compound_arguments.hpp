36bdfe4a55d2771eac0c29613b9508d2fe2e8178
Stephan van Veen <vanveen.stephan@gmail.com>
2019-05-18 14:04:57 +0200

    Change test cases

--- test/test_compound_arguments.hpp
+++ test/test_compound_arguments.hpp
@@ -77,30 +77,7 @@ TEST_CASE("Parse compound toggle arguments with implicit values and nargs and ot
   program.add_argument("--input_files")
     .nargs(3);
 
-  program.parse_args({ "./test.exe", "1", "-abc", "3.14", "2.718", "2", "--input_files",
-    "a.txt", "b.txt", "c.txt", "3" });
-
-  REQUIRE(program.get<bool>("-a") == true);
-  REQUIRE(program.get<bool>("-b") == true);
-  auto c = program.get<std::vector<float>>("-c");
-  REQUIRE(c.size() == 2);
-  REQUIRE(c[0] == 3.14f);
-  REQUIRE(c[1] == 2.718f);
-  auto input_files = program.get<std::vector<std::string>>("--input_files");
-  REQUIRE(input_files.size() == 3);
-  REQUIRE(input_files[0] == "a.txt");
-  REQUIRE(input_files[1] == "b.txt");
-  REQUIRE(input_files[2] == "c.txt");
-  auto numbers = program.get<std::vector<int>>("numbers");
-  REQUIRE(numbers.size() == 3);
-  REQUIRE(numbers[0] == 1);
-  REQUIRE(numbers[1] == 2);
-  REQUIRE(numbers[2] == 3);
-  auto numbers_list = program.get<std::list<int>>("numbers");
-  REQUIRE(numbers.size() == 3);
-  REQUIRE(testutility::get_from_list(numbers_list, 0) == 1);
-  REQUIRE(testutility::get_from_list(numbers_list, 1) == 2);
-  REQUIRE(testutility::get_from_list(numbers_list, 2) == 3);
+  REQUIRE_THROWS(program.parse_args({ "./test.exe", "1", "-abc", "3.14", "2.718", "2", "--input_files", "a.txt", "b.txt", "c.txt", "3" }));
 }
 
 TEST_CASE("Parse out-of-order compound arguments", "[compound_arguments]") {
