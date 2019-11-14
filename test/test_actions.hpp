f6e686b69c4452489cffef11a1c29c5ee03aaf28
Zhihao Yuan <lichray@gmail.com>
2019-11-14 01:30:04 -0600

    Bind extra arguments to actions
    closes: p-ranav/argparse#38

--- test/test_actions.hpp
+++ test/test_actions.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <catch.hpp>
 #include <argparse.hpp>
+#include <string_view>
 
 TEST_CASE("Users can use default value inside actions", "[actions]") {
   argparse::ArgumentParser program("test");
@@ -37,5 +38,84 @@ TEST_CASE("Users can add actions that return nothing", "[actions]") {
     program.parse_args({"test", "ignored"});
     REQUIRE(pressed);
     REQUIRE(program.get<int>("button") == 42);
+  }
+}
+
+class Image {
+public:
+  int w = 0, h = 0;
+
+  void resize(std::string_view geometry) {
+    std::stringstream s;
+    s << geometry;
+    s >> w;
+    s.ignore();
+    s >> h;
+  }
+
+  static auto create(int w, int h, std::string_view format) -> Image {
+    auto factor = [=] {
+      if (format == "720p")
+        return std::min(1280. / w, 720. / h);
+      else if (format == "1080p")
+        return std::min(1920. / w, 1080. / h);
+      else
+        return 1.;
+    }();
+
+    return {static_cast<int>(w * factor), static_cast<int>(h * factor)};
+  }
+};
+
+TEST_CASE("Users can bind arguments to actions", "[actions]") {
+  argparse::ArgumentParser program("test");
+
+  GIVEN("an default initialized object bounded by reference") {
+    Image img;
+    program.add_argument("--size").action(&Image::resize, std::ref(img));
+
+    WHEN("provided no command-line arguments") {
+      program.parse_args({"test"});
+
+      THEN("the object is not updated") {
+        REQUIRE(img.w == 0);
+        REQUIRE(img.h == 0);
+      }
+    }
+
+    WHEN("provided command-line arguments") {
+      program.parse_args({"test", "--size", "320x98"});
+
+      THEN("the object is updated accordingly") {
+        REQUIRE(img.w == 320);
+        REQUIRE(img.h == 98);
+      }
+    }
+  }
+
+  GIVEN("a command-line option waiting for the last argument in its action") {
+    program.add_argument("format").action(Image::create, 400, 300);
+
+    WHEN("provided such an argument") {
+      program.parse_args({"test", "720p"});
+
+      THEN("the option object is created as if providing all arguments") {
+        auto img = program.get<Image>("format");
+
+        REQUIRE(img.w == 960);
+        REQUIRE(img.h == 720);
+      }
+    }
+
+    WHEN("provided a different argument") {
+      program.parse_args({"test", "1080p"});
+
+      THEN("a different option object is created") {
+        auto img = program.get<Image>("format");
+
+        REQUIRE(img.w == 1440);
+        REQUIRE(img.h == 1080);
+      }
+    }
   }
 }
