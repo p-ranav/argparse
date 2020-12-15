51dd98f49df073268977e695fa15b82ceabf91b1
Mike Zozu <jaat-quod@yandex.ru>
2020-12-15 21:59:52 +0300

    fix is_streamable trait

--- include/argparse/argparse.hpp
+++ include/argparse/argparse.hpp
@@ -73,7 +73,7 @@ struct is_streamable : std::false_type {};
 
 template <typename T>
 struct is_streamable<
-    T, std::void_t<decltype(std::declval<std::ostream>() << std::declval<T>())>>
+    T, std::void_t<decltype(std::declval<std::ostream&>() << std::declval<T>())>>
   : std::true_type {};
 
 template <typename T>
