4f10f378c526c8570288daa92e269a26c6ea07ad
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2022-09-20 21:20:55 -0700

    Bumped version to v2.8

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class ArgparseConan(ConanFile):
     name = "argparse"
-    version = "2.7"
+    version = "2.8"
     exports_sources = "include/argparse.hpp"
     no_copy_source = True
 
