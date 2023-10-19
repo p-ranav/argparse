aa25a2f3acc8eda57e0b392e5e59e9a69648531a
Kian-Meng Ang <kianmeng@cpan.org>
2023-10-19 13:09:11 +0800

    Fix typos
    Found via `codespell -L seh`

--- include/argparse/argparse.hpp
+++ include/argparse/argparse.hpp
@@ -1324,7 +1324,7 @@ public:
   }
 
   /* Indexing operator. Return a reference to an Argument object
-   * Used in conjuction with Argument.operator== e.g., parser["foo"] == true
+   * Used in conjunction with Argument.operator== e.g., parser["foo"] == true
    * @throws std::logic_error in case of an invalid argument name
    */
   Argument &operator[](std::string_view arg_name) const {
