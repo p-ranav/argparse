8f70dde82e942cf0d4e7332b6e8ae752b540cb0f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2023-11-04 15:20:47 -0500

    Added unit test for mutex_args with three arguments

--- test/test_mutually_exclusive_group.cpp
+++ test/test_mutually_exclusive_group.cpp
@@ -37,3 +37,18 @@ TEST_CASE(
       "Argument '--second VAR' not allowed with '--first VAR'",
       std::runtime_error);
 }
+
+TEST_CASE("Create mutually exclusive group with 3 arguments" *
+          test_suite("mutex_args")) {
+  argparse::ArgumentParser program("test");
+
+  auto &group = program.add_mutually_exclusive_group();
+  group.add_argument("--first");
+  group.add_argument("--second");
+  group.add_argument("--third");
+
+  REQUIRE_THROWS_WITH_AS(
+      program.parse_args({"test", "--first", "1", "--third", "2"}),
+      "Argument '--third VAR' not allowed with '--first VAR'",
+      std::runtime_error);
+}
